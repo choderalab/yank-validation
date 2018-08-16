c10c900cd8fe33599c1f16950ff51aec43cbac18
John Chodera <john.chodera@choderalab.org>
2018-08-16 12:01:22 -0400

    Update input files for repex vs sams comparison of CB7 validation ligands

--- cucurbit7uril/run-lsf-repex.sh
+++ cucurbit7uril/run-lsf-repex.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
-#Walltime
-#BSUB -W 24:00
+# Set walltime limit
+#BSUB -W 72:00
 #
-# Set Output file
-#BSUB -o  host-guest.%J.log
+# Set output file
+#BSUB -o repex.%J.log
 #
 # Specify node group
-#BSUB -m "ls-gpu lt-gpu"
+#BSUB -m "ls-gpu lt-gpu lp-gpu lg-gpu"
 #BSUB -q gpuqueue
 #
 # nodes: number of nodes and GPU request
-#BSUB -n 3 -R "rusage[mem=12]"
-#BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
+#BSUB -n 8
+#BSUB -gpu "num=4:j_exclusive=yes:mode=shared" -R "rusage[mem=36] span[ptile=4]"
 #
 # job name (default = name of script file)
-#BSUB -J "host-guest"
+#BSUB -J "repex"
 
-echo "LSB_HOSTS: $LSB_HOSTS"
-echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-module add cuda/9.0
-nvcc --version
-
-build_mpirun_configfile "yank script --yaml=cucurbit7uril-repex.yaml"
-
-blaunch -u hostfile$JOBID "module list"
-blaunch -u hostfile$JOBID "nvidia-smi"
-
-mpiexec.hydra -f hostfile -configfile configfile
-
-blaunch -u hostfile$JOBID "nvidia-smi"
+build_mpirun_configfile --hostfilepath hostfile.${LSB_JOBNAME} --configfilepath configfile.${LSB_JOBNAME} "yank script --yaml=${LSB_JOBNAME}.yaml"
+mpiexec.hydra -f hostfile.${LSB_JOBNAME} -configfile configfile.${LSB_JOBNAME}
