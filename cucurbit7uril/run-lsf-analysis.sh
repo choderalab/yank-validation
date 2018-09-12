ef2917c432b3db914f823a5fce5ef483621ca456
John Chodera <john.chodera@choderalab.org>
2018-09-11 23:16:08 -0400

    Add example pickle files

--- cucurbit7uril/run-lsf-analysis.sh
+++ cucurbit7uril/run-lsf-analysis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Set walltime limit
-#BSUB -W 5:59
+#BSUB -W 12:00
 #
 # Set output file
 #BSUB -o analyze.%J.log
@@ -10,11 +10,13 @@
 #BSUB -q cpuqueue
 #
 # nodes: number of nodes and GPU request
-#BSUB -n 4
-#BSUB -R "rusage[mem=48] span[ptile=4]"
+#BSUB -n 9
+#BSUB -R "rusage[mem=48] span[hosts=1]"
 #
 # job name (default = name of script file)
 #BSUB -J "analyze"
 
-yank analyze report --yaml sams.yaml --skipunbiasing --report --output /data/chodera/chodera/2018-08-05/yank-validation/cucurbit7uril/report/sams --serial=sams.pkl --fulltraj
-yank analyze report --yaml repex.yaml --skipunbiasing --report --output /data/chodera/chodera/2018-08-05/yank-validation/cucurbit7uril/report/repex --serial=repex.pkl --fulltraj
+mpirun -np 9 yank analyze --yaml sams.yaml --serial=sams.pkl
+mpirun -np 9 yank analyze --yaml repex.yaml --serial=repex.pkl
+mpirun -np 9 yank analyze report --yaml sams.yaml --report --output /data/chodera/chodera/2018-08-05/yank-validation/cucurbit7uril/report/sams --serial=sams.pkl
+mpirun -np 9 yank analyze report --yaml repex.yaml --report --output /data/chodera/chodera/2018-08-05/yank-validation/cucurbit7uril/report/repex --serial=repex.pkl
